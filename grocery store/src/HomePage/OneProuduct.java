90c505e27774d7dc37f0b0da8aa23b17e513a015
Mobina8204 <mobinamohammadi.mh@gmail.com>
2023-07-06 12:44:16 +0330

    image of products

--- grocery store/src/HomePage/OneProuduct.java
+++ grocery store/src/HomePage/OneProuduct.java
@@ -1,9 +1,15 @@
 package HomePage;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 public class OneProuduct {
@@ -18,17 +24,23 @@ public class OneProuduct {
     public Label rate ;
     
     @FXML
-    private AnchorPane holePane ;
+    private AnchorPane wholePane ;
     
+    @FXML
+    private Label imageLable;
+
+    @FXML
+    private ImageView productImage;
+
     @FXML
     private Spinner<Integer> spinner;
     
     public Label getPrice(){return price;}
     public Label getProductNumber(){return prouductName;}
     public Label getRate(){return rate;}
-    public AnchorPane getHolePane(){return holePane;}
+    public AnchorPane getWholePane(){return wholePane;}
 
-    public OneProuduct makeOneProuduct (String price, String name, double rate , int max){
+    public OneProuduct makeOneProuduct (String price, String name, double rate , int max , String imagePath) throws FileNotFoundException{
         this.price.setText(price);
         this.prouductName.setText(name);
         this.rate.setText(Double.toString(rate));
@@ -36,6 +48,11 @@ public class OneProuduct {
         spinner.setValueFactory(
             new SpinnerValueFactory.IntegerSpinnerValueFactory(0, max)
         );
+        //set image
+        imageLable.setVisible(false);
+        InputStream stream = new FileInputStream(imagePath);
+        Image image = new Image(stream);
+        productImage.setImage(image);
 
         return (this);
     }
